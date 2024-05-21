902ee2c89cafc07c65d01807ae5d7b86bfc83f92
Syed Farhan <syedfarhan596@gmail.com>
2024-05-21 15:58:14 +0530

    remove server unused

--- src/server-url.js
+++ src/server-url.js
@@ -1,5 +1,3 @@
 export const userUrl = "https://cfdi-server.onrender.com/api/v1/user";
 
 export const adminUrl = "https://cfdi-server.onrender.com/api/v1/admin";
-
-export const server = "https://cfdi-server.onrender.com";
