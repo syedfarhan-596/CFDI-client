39d36802edc6f0da376b920579434a23cd1a0e55
Syed Farhan <syedfarhan596@gmail.com>
2024-06-27 01:05:19 +0530

    removed links

--- src/server-url.js
+++ src/server-url.js
@@ -1,5 +1,3 @@
-// export const userUrl = "https://cfdi-server.onrender.com/api/v1/user";
 export const userUrl = process.env.REACT_APP_USER_URL;
 
 export const adminUrl = process.env.REACT_APP_ADMIN_URL;
-// export const adminUrl = "https://cfdi-server.onrender.com/api/v1/admin";
