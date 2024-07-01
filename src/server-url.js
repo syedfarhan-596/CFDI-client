caff644c14faa10edce1d7d562078dfd82bd185f
syedfarhan <syedfarhan596@gmail.com>
2024-07-01 15:54:18 +0530

    finall

--- src/server-url.js
+++ src/server-url.js
@@ -1,9 +1,9 @@
-// export const userUrl = process.env.REACT_APP_USER_URL;
+export const userUrl = process.env.REACT_APP_USER_URL;
 
-// export const adminUrl = process.env.REACT_APP_ADMIN_URL;
+export const adminUrl = process.env.REACT_APP_ADMIN_URL;
 
-// export const url = process.env.REACT_APP_URL;
+export const url = process.env.REACT_APP_URL;
 
-export const userUrl = "http://localhost:4000/api/v1/user";
-export const adminUrl = "http://localhost:4000/api/v1/admin";
-export const url = "http://localhost:4000/";
+// export const userUrl = "http://localhost:4000/api/v1/user";
+// export const adminUrl = "http://localhost:4000/api/v1/admin";
+// export const url = "http://localhost:4000/";
